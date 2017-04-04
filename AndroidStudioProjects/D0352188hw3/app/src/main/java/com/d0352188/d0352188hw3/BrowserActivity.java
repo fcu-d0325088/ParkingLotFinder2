321f71b8f8df574c0c39932a2990847a663d8453
DESKTOP-E864U5D\sky_h <sky_high981@hotmail.com>
2017-04-04 08:26:04 +0800

    homework 3

---
+++ AndroidStudioProjects/D0352188hw3/app/src/main/java/com/d0352188/d0352188hw3/BrowserActivity.java
@@ -0,0 +1,13 @@
+package com.d0352188.d0352188hw3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrowserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browser);
+    }
+}
